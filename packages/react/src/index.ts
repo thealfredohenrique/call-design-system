3400ea15895e44f5d373b99dc6f106eeb7dd7385
Alfredo Henrique <thealfredohenrique@gmail.com>
2023-08-15 20:53:24 -0300

    feat(config): Add React package and initial configuration

--- packages/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { colors } from '@call/tokens'
-
-console.log('colors', colors)
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,5 @@
+import { colors } from '@call/tokens'
+
+export function App() {
+  return <h1 style={{ color: colors.ignite300 }}>Hello world.</h1>
+}
