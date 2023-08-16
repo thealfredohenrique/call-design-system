f8cbfa6b9d6f3cd8ae2c1bbb45ced1c21e29371a
Alfredo Henrique <thealfredohenrique@gmail.com>
2023-08-15 21:06:02 -0300

    feat(config): Add Stitches package and initial configuration

---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,36 @@
+import { createStitches, defaultThemeMap } from '@stitches/react'
+import {
+  colors,
+  fontSizes,
+  fontWeights,
+  fonts,
+  lineHeights,
+  radii,
+  space,
+} from '@call/tokens'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
+  },
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space',
+  },
+})
