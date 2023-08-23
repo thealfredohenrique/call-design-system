fe79f3fe8b1e38c01b2d4008235bac76cb2e2053
Alfredo Henrique <thealfredohenrique@gmail.com>
2023-08-22 21:31:08 -0300

    docs(storybook): Add tokens documentation

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@ import type { StoryObj, Meta } from '@storybook/react'
 import { Button, ButtonProps } from '@call/react'
 
 export default {
-  title: 'Button',
+  title: 'Form/Button',
   component: Button,
   args: {
     children: 'Enviar',
